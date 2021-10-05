8f4b00ef020957752b243da94e52ecac170b7a71
alexsincai <alex.sincai@yahoo.co.uk>
2021-10-05 09:34:54 +0300

    genres to state

--- src/stateManager.js
+++ src/stateManager.js
@@ -1,7 +1,35 @@
 import { atom } from "jotai";
 
-// import services from "./data/services.json";
-import genres from "./data/genres.json";
+const genres = [
+    { id: 28, name: "Action" },
+    { id: 7, name: "Adult" },
+    { id: 12, name: "Adventure" },
+    { id: 16, name: "Animation" },
+    { id: 1, name: "Biography" },
+    { id: 35, name: "Comedy" },
+    { id: 80, name: "Crime" },
+    { id: 99, name: "Documentary" },
+    { id: 18, name: "Drama" },
+    { id: 10751, name: "Family" },
+    { id: 14, name: "Fantasy" },
+    { id: 2, name: "Film Noir" },
+    { id: 3, name: "Game Show" },
+    { id: 36, name: "History" },
+    { id: 27, name: "Horror" },
+    { id: 10402, name: "Music" },
+    { id: 4, name: "Musical" },
+    { id: 9648, name: "Mystery" },
+    { id: 10763, name: "News" },
+    { id: 10764, name: "Reality" },
+    { id: 10749, name: "Romance" },
+    { id: 878, name: "Science Fiction" },
+    { id: 6, name: "Short" },
+    { id: 5, name: "Sport" },
+    { id: 10767, name: "Talk Show" },
+    { id: 53, name: "Thriller" },
+    { id: 10752, name: "War" },
+    { id: 37, name: "Western" },
+];
 
 const countries = [
     "ae",
