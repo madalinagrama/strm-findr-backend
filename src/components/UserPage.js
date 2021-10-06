a96ce725581fdbaa5f67fcc8a58aa03b44820c23
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2021-10-06 16:17:31 +0300

    duplicate import

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const UserPage = () => {
     const [users, setUsers] = useState([]);
