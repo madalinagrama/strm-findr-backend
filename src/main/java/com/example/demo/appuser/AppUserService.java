a5bb7774215371a0c9456ae33ad113b047ae7fa7
Madalina <madalina@Madalinas-MacBook-Air.local>
2021-10-06 16:35:23 +0300

    [security] - refactor AppUseController with information from authController and deleted that one

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -2,6 +2,7 @@ package com.example.demo.appuser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
 
@@ -23,9 +24,9 @@ public class AppUserService {
         return appUserRepository.findAll();
     }
 
-    public AppUser findById(Long id) {
-        return appUserRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Could not find user with username: " + id));
+    public AppUser findByUserName(String username) {
+        return appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Could not find user with username: " + username));
     }
 
     public Optional<AppUser> getUser(Long id) {
@@ -49,17 +50,43 @@ public class AppUserService {
         appUserRepository.deleteById(id);
     }
 
-    public void updateUser(Long id, AppUser appUser) {
-        boolean exists = appUserRepository.existsById(id);
-        if (!exists) {
-            throw new IllegalStateException("User with id " + id + " does not exist!");
-        }
-        AppUser user = appUserRepository.getById(id);
+    public AppUser updateUser(Long id, String username, AppUser appUser) {
+        AppUser user = validateNewUserNameAndEmail(id, username, appUser.getUsername(), appUser.getEmail());
         user.setFirstName(appUser.getFirstName());
         user.setLastName(appUser.getLastName());
         user.setUsername(appUser.getUsername());
         user.setEmail(appUser.getEmail());
         user.setPassword(appUser.getPassword());
+        user.setJoinedDate(appUser.getJoinedDate());
+        user.setRoles(appUser.getRoles());
         appUserRepository.save(user);
+        return user;
+    }
+
+    public AppUser validateNewUserNameAndEmail(Long id, String currentUsername, String newUsername, String newEmail) {
+        AppUser userByNewUsername = appUserRepository.findUserByUsername(newUsername);
+        AppUser userByNewEmail = appUserRepository.findUserByEmail(newEmail);
+        if (StringUtils.isNotBlank(currentUsername)) {
+            AppUser currentUser = findByUserName(currentUsername);
+            if (currentUser == null) {
+                throw new IllegalArgumentException("No user found by username: " + currentUsername + "!");
+            }
+            if (userByNewUsername != null && !currentUser.getId().equals(userByNewUsername.getId())) {
+                throw new IllegalArgumentException("Username already exists!");
+            }
+            if (userByNewEmail != null && !currentUser.getId().equals(userByNewEmail.getId())) {
+                throw new IllegalArgumentException("Email already exists!");
+            }
+            return currentUser;
+        } else {
+            if (userByNewUsername != null) {
+                throw new IllegalArgumentException("Username already exists!");
+            }
+            if (userByNewEmail != null) {
+                throw new IllegalArgumentException("Email already exists!");
+            }
+            return null;
+
+        }
     }
 }
