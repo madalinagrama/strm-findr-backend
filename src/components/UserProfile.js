33508b8bf7229b59de4665b3a9dfd41ba66aaf36
alexsincai <alex.sincai@yahoo.co.uk>
2021-10-19 21:01:52 +0300

    oops

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,5 @@
 import { useAtom } from "jotai";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import state from "../stateManager";
 import { getUser } from "./user/service/UserService";
 
