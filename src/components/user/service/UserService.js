6053015fe6052e84522a1c90fb033a8647ae5ed7
madalinagrama <m.madalinagrama@gmail.com>
2021-10-19 13:37:59 +0300

    [user-profile] - correct endpoint for user profile

--- src/components/user/service/UserService.js
+++ src/components/user/service/UserService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "../../auth/components/services/auth-header";
 
-const API_URL = process.env.REACT_APP_BASE_URL;
+const API_URL = process.env.REACT_APP_AUTH_URL;
 
 export const getUser = (username) => {
     let auth = authHeader();
